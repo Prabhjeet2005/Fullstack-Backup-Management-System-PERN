967ac8e5cc8dc0b8df89e1827efa28fb1f2b89e8
Prabhjeet <prabhjeetsandhu010@gmail.com>
2025-09-06 23:52:04 +0530

    feat: Table Data Restructured

--- server/models/postrgeSQLCommands.models.sql
+++ server/models/postrgeSQLCommands.models.sql
@@ -1,19 +1,20 @@
 
 CREATE TABLE IF NOT EXISTS roles(
-  id SERIAL PRIMARY KEY,
-  role_name VARCHAR(255) UNIQUE NOT NULL
+  id SERIAL,
+  role_name VARCHAR(255) UNIQUE NOT NULL PRIMARY KEY
 );
 
 CREATE TABLE IF NOT EXISTS users(
   id SERIAL PRIMARY KEY,
   email VARCHAR(255) UNIQUE NOT NULL,
   username VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL,
-  role_id INTEGER NOT NULL REFERENCES roles(id),
+  password_hashed VARCHAR(255) NOT NULL,
+  role_name VARCHAR(255) NOT NULL REFERENCES roles(role_name),
   created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
 INSERT INTO roles(role_name) VALUES ('Admin');
 INSERT INTO  roles(role_name) VALUES ('Auditor');
 
-CREATE INDEX idx_id ON users(id);
+-- INSERT INTO users(email,username,password_hashed,role_name) VALUES('user@gmail.com','user','Qwerty@123','Auditor');
+-- INSERT INTO users(email,username,password_hashed,role_name) VALUES('admin@gmail.com','admin','Qwerty@123','Admin');
