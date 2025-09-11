1eeef917c98846d1f672cbaa2f007833c5e6db9c
Prabhjeet <prabhjeetsandhu010@gmail.com>
2025-09-11 17:51:00 +0530

    backups Table Added to store metadeta about each backup

--- server/models/postrgeSQLCommands.models.sql
+++ server/models/postrgeSQLCommands.models.sql
@@ -15,6 +15,15 @@ CREATE TABLE IF NOT EXISTS users(
 
 INSERT INTO roles(role_name) VALUES ('Admin');
 INSERT INTO  roles(role_name) VALUES ('Auditor');
+INSERT INTO users(email,username,password_hashed,role_name) VALUES('user@gmail.com','user','Qwerty@123','Auditor');
+INSERT INTO users(email,username,password_hashed,role_name) VALUES('admin@gmail.com','admin','Qwerty@123','Admin');
 
--- INSERT INTO users(email,username,password_hashed,role_name) VALUES('user@gmail.com','user','Qwerty@123','Auditor');
--- INSERT INTO users(email,username,password_hashed,role_name) VALUES('admin@gmail.com','admin','Qwerty@123','Admin');
+CREATE TABLE IF NOT EXISTS backups (
+    id SERIAL PRIMARY KEY,
+    file_name VARCHAR(255) NOT NULL,
+    file_path VARCHAR(255) NOT NULL,
+    file_size_bytes BIGINT NOT NULL,
+    created_by_user_id INTEGER NOT NULL REFERENCES users(id),
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
