301575bbe5251b718ef97552974b5cba24e2c0f3
Prabhjeet <prabhjeetsandhu010@gmail.com>
2025-09-11 14:23:26 +0530

    Making Protected Route To Display Pages only to Authoized users

---
+++ client/src/components/ProtectedRoute.jsx
@@ -0,0 +1,19 @@
+import React, { useContext } from "react";
+import { UserContext } from "../context/UserContext";
+import { Navigate, useLocation } from "react-router-dom";
+import Loader from "./Loader";
+
+const ProtectedRoute = ({ children }) => {
+	const { isLoggedIn, isLoading } = useContext(UserContext);
+	const location = useLocation();
+
+	if (isLoading) {
+		return <Loader isLoading={isLoading} />;
+	}
+	if (!isLoggedIn) {
+		return <Navigate to={"/login"} state={{ from: location }} replace />; 
+	}
+	return children; // Actual  JSX stored in children, but {children} send multiple data bundled together
+};
+
+export default ProtectedRoute;
