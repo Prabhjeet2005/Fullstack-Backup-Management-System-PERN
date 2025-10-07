8f4058299db09e79dc0525cb49eb228ccf27fb71
Prabhjeet <prabhjeetsandhu010@gmail.com>
2025-10-08 00:27:51 +0530

    Updated Server.js

--- server/server.js
+++ server/server.js
@@ -1,31 +1,44 @@
-require("dotenv").config()
-const express = require("express")
-const cookieParser = require("cookie-parser")
-const cors = require("cors")
-const { errorHandler } = require("./utils/responseCreator")
-const apiHealth = require("./routes/apiHealth.route")
+// --- START OF DEBUGGING BLOCK ---
+// We add this here to see the environment variables BEFORE anything else runs.
+require("dotenv").config();
 
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const { errorHandler } = require("./utils/responseCreator");
+const apiHealth = require("./routes/apiHealth.route");
 
-const authRouter = require("./routes/auth.route")
-const backupRouter = require("./routes/backup.route")
+const authRouter = require("./routes/auth.route");
+const backupRouter = require("./routes/backup.route");
+const autoBackupRouter = require("./routes/autoBackup.route");
+const { startScheduler } = require("./utils/schedulesHelper.util");
 
-const app = express()
-app.use(express.json())
-app.use(cookieParser())
-app.use(cors({
-  origin:`${process.env.CLIENT_URL}`,
-  credentials:true,
-  methods:["GET","POST","PATCH","DELETE"]
-}))
+const startServer = async()=>{
 
-app.use("/api/check-api-health",apiHealth);
-app.use("/api/auth",authRouter)
-app.use("/api/backups",backupRouter)
+const app = express();
 
-app.use(errorHandler)
+await startScheduler()
 
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: `${process.env.CLIENT_URL}`,
+    credentials: true,
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+  })
+);
+
+app.use("/api/check-api-health", apiHealth);
+app.use("/api/auth", authRouter);
+app.use("/api/backups", backupRouter);
+app.use("/api/schedule-auto-backup", autoBackupRouter);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 7000;
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
   console.log(`Server Running on ${PORT}`);
-})
+});
+}
+startServer()
