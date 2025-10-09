020504732b02bbee1716db7b872ba3b7eece1b3e
Prabhjeet <prabhjeetsandhu010@gmail.com>
2025-10-10 02:49:15 +0530

    Updated Code For Production

--- server/server.js
+++ server/server.js
@@ -22,10 +22,7 @@ const startServer = async()=>{
   app.use(cookieParser());
   app.use(
     cors({
-      origin:
-        process.env.NODE_ENV === "production"
-          ? "http://localhost:3000"
-          : `${process.env.CLIENT_URL}`,
+      origin:process.env.CLIENT_URL,      
       credentials: true,
       methods: ["GET", "POST", "PATCH", "DELETE"],
     })
@@ -37,16 +34,6 @@ const startServer = async()=>{
   app.use("/api/schedule-auto-backup", autoBackupRouter);
 
   app.use(errorHandler);
-
-  // --- Static File Serving ---
-  // Serve the static files from the React app
-  app.use(express.static(path.join(__dirname, "public")));
-
-  // --- Catch-All Route ---
-  // This route serves the React app's index.html for any request that doesn't match an API route
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
-  });
 
   const PORT = process.env.PORT || 7000;
   // app.listen(PORT, () => {
