0be8f3a82c9a3785a3d30d2af1cee9667c610853
Prabhjeet <prabhjeetsandhu010@gmail.com>
2025-10-10 02:21:24 +0530

    Production Build

--- server/server.js
+++ server/server.js
@@ -14,32 +14,44 @@ const autoBackupRouter = require("./routes/autoBackup.route");
 const { startScheduler } = require("./utils/schedulesHelper.util");
 
 const startServer = async()=>{
+  const app = express();
 
-const app = express();
+  await startScheduler();
 
-await startScheduler()
+  app.use(express.json());
+  app.use(cookieParser());
+  app.use(
+    cors({
+      origin:
+        process.env.NODE_ENV === "production"
+          ? "http://localhost:3000"
+          : `${process.env.CLIENT_URL}`,
+      credentials: true,
+      methods: ["GET", "POST", "PATCH", "DELETE"],
+    })
+  );
 
-app.use(express.json());
-app.use(cookieParser());
-app.use(
-  cors({
-    origin: process.env.NODE_ENV==="production"?"http://localhost:3000":`${process.env.CLIENT_URL}`,
-    credentials: true,
-    methods: ["GET", "POST", "PATCH", "DELETE"],
-  })
-);
+  app.use("/api/check-api-health", apiHealth);
+  app.use("/api/auth", authRouter);
+  app.use("/api/backups", backupRouter);
+  app.use("/api/schedule-auto-backup", autoBackupRouter);
 
-app.use("/api/check-api-health", apiHealth);
-app.use("/api/auth", authRouter);
-app.use("/api/backups", backupRouter);
-app.use("/api/schedule-auto-backup", autoBackupRouter);
+  app.use(errorHandler);
 
-app.use(errorHandler);
+  // --- Static File Serving ---
+  // Serve the static files from the React app
+  app.use(express.static(path.join(__dirname, "public")));
 
-const PORT = process.env.PORT || 7000;
-// app.listen(PORT, () => {
-//   console.log(`Server Running on ${PORT}`);
-// });
+  // --- Catch-All Route ---
+  // This route serves the React app's index.html for any request that doesn't match an API route
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+  });
+
+  const PORT = process.env.PORT || 7000;
+  // app.listen(PORT, () => {
+  //   console.log(`Server Running on ${PORT}`);
+  // });
 }
 startServer()
 
