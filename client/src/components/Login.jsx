e1d1dad41aea43921e27f6995ce474ab72a81738
Prabhjeet <prabhjeetsandhu010@gmail.com>
2025-10-09 23:06:40 +0530

    Login Signup Page Improvement + Color Change

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -73,98 +73,110 @@ const Login = () => {
 		}
 	};
 	return (
-		<>
-			<Container fluid>
-				<Row>
-					<Col xs={{ span: 10, offset: 1 }} md={{ span: 8, offset: 2 }}>
-						<Row className="d-flex flex-column auth-container">
-							<Col
-								style={{ color: `#008080` }}
-								className="text-center auth-heading">
-								Login
-							</Col>
+    <>
+      <Container fluid>
+        <Row>
+          <Col xs={{ span: 10, offset: 1 }} md={{ span: 8, offset: 2 }}>
+            <Row
+              style={{ marginTop: `30vh` }}
+              className="d-flex flex-column auth-container"
+            >
+              <Col
+                style={{ color: `#008080` }}
+                className="text-center auth-heading"
+              >
+                Login
+              </Col>
 
-							<Col>
-								<Row className="auth-field-container">
-									<Col className="auth-text">Email</Col>
-									<Col
-										xs={{ span: 7 }}
-										sm={{ span: 9 }}
-										// md={{ span: 10 }}
-										className="auth-input relative">
-										<span className="relative">
-											{" "}
-											<input
-												required
-												onChange={handleEmailChange}
-												type="text"
-												placeholder="Enter Email"
-											/>
-											<img
-												src="./images/email-8-svgrepo-com (1).svg"
-												className="absolute img-auth"
-												alt="username"
-											/>
-										</span>
-									</Col>
-								</Row>
-							</Col>
-							<Col>
-								<Row className="auth-field-container">
-									<Col className="auth-text">Password</Col>
-									<Col
-										xs={{ span: 7 }}
-										sm={{ span: 9 }}
-										// md={{ span: 10 }}
-										className="auth-input relative">
-										<span className="relative">
-											{" "}
-											<input
-												required
-												onChange={handlePasswordChange}
-												type={passwordVisible ? `text` : `password`}
-												placeholder="Enter Password"
-											/>
-											<img
-												onClick={(e) => setPasswordVisible(!passwordVisible)}
-												src={
-													passwordVisible
-														? "./images/eye-open-svgrepo-com.svg"
-														: "./images/eye-close-svgrepo-com.svg"
-												}
-												className="absolute img-auth"
-												alt="password"
-											/>
-										</span>
-									</Col>
-								</Row>
-							</Col>
-							<Button
-								disabled={isLoading}
-								onClick={handleSubmit}
-								className={`auth-btn ${isLoading && "btn-loading"}`}>
-								{isLoading ? (
-									<img
-										className="img-loader-btn"
-										src="./images/block-2-svgrepo-com (1).svg"
-										alt="disabled img"
-									/>
-								) : (
-									<span>Login</span>
-								)}
-							</Button>
-							<Col
-								onClick={() => navigate("/signup",{replace:true})}
-								style={{ color: `#008080` }}
-								className="text-center redirect ">
-								Not A User? Signup Now
-							</Col>
-						</Row>
-					</Col>
-				</Row>
-			</Container>
-		</>
-	);
+              <Col>
+                <Row className="auth-field-container flex-column flex-sm-row gap-2 gap-sm-0">
+                  <Col className="auth-text">Email</Col>
+                  <Col
+                    xs={{ span: 7 }}
+                    sm={{ span: 9 }}
+                    // md={{ span: 10 }}
+                    className="auth-input relative"
+                  >
+                    <span className="relative">
+                      {" "}
+                      <input
+											className="w-100"
+                        required
+                        onChange={handleEmailChange}
+                        type="text"
+                        placeholder="Enter Email"
+                      />
+                      <img
+                        src="./images/email-8-svgrepo-com (1).svg"
+                        className="absolute img-auth"
+                        alt="username"
+                      />
+                    </span>
+                  </Col>
+                </Row>
+              </Col>
+              <Col>
+                <Row className="auth-field-container  flex-column flex-sm-row gap-2 gap-sm-0">
+                  <Col className="auth-text">Password</Col>
+                  <Col
+                    xs={{ span: 7 }}
+                    sm={{ span: 9 }}
+                    // md={{ span: 10 }}
+                    className="auth-input relative"
+                  >
+                    <span className="relative">
+                      {" "}
+                      <input
+                        required
+												className="w-100"
+                        onChange={handlePasswordChange}
+                        type={passwordVisible ? `text` : `password`}
+                        placeholder="Enter Password"
+                      />
+                      <img
+                        onClick={(e) =>
+                          setPasswordVisible(!passwordVisible)
+                        }
+                        src={
+                          passwordVisible
+                            ? "./images/eye-open-svgrepo-com.svg"
+                            : "./images/eye-close-svgrepo-com.svg"
+                        }
+                        className="absolute img-auth"
+                        alt="password"
+                      />
+                    </span>
+                  </Col>
+                </Row>
+              </Col>
+              <Button
+                disabled={isLoading}
+                onClick={handleSubmit}
+                className={`auth-btn ${isLoading && "btn-loading"}`}
+              >
+                {isLoading ? (
+                  <img
+                    className="img-loader-btn"
+                    src="./images/block-2-svgrepo-com (1).svg"
+                    alt="disabled img"
+                  />
+                ) : (
+                  <span>Login</span>
+                )}
+              </Button>
+              <Col
+                onClick={() => navigate("/signup", { replace: true })}
+                style={{ color: `#008080` }}
+                className="text-center redirect "
+              >
+                Not A User? Signup Now
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 };
 
 export default Login;
