62629d85a0fb0b1fc8320631fe0ba5a75da72512
Prabhjeet <prabhjeetsandhu010@gmail.com>
2025-10-10 01:58:22 +0530

    Removed Escape Character From Regex {Hindering Production}

--- client/src/utils/regex.js
+++ client/src/utils/regex.js
@@ -1,5 +1,5 @@
 export const nameRegex = /^[a-zA-Z]+$/;
-export const emailRegex = /^[a-zA-Z0-9]+\@[a-zA-Z]+\.[a-z]{2,4}$/;
+export const emailRegex = /^[a-zA-Z0-9]+@[a-zA-Z]+\.[a-z]{2,4}$/;
 export const lowercaseRegex = /[a-z]/;
 export const uppercaseRegex = /[A-Z]/;
 export const specialCharRegex = /[\W_]/;
